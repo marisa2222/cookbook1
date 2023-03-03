4ccbc0167798e742fd4abb5e55db97f4e8047b69
marisa2222 <msharifi2222@gmail.com>
2023-03-03 10:42:19 +0100

    added 27 files

---
+++ actions-menu.js
@@ -0,0 +1,14 @@
+
+function shareEvent(){
+    alert(' Hello everyone! Choose your food and Happy Yummy!')
+}
+
+
+document.getElementById("navBarJs").addEventListener("click", shareEvent);
+
+
+function DragEvent(){
+    alert('my alert message')
+}
+
+document.getElementById("share-button").addEventListener("click", DragEvent);
